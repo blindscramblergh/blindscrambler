b2227a9763fbde49cd333276a5eef8618ba8d668
blindscramblergh <blindscrambler@gmail.com>
2025-10-01 17:07:26 -0500

    commit during oct, 1 class

---
+++ src/blindscrambler/model/regression.py
@@ -0,0 +1,30 @@
+# Author metadata
+
+__Name__ = "Syed Raza"
+__email__ = "sar0033@uah.edu"
+
+# import statements
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+
+# make a Linear Regression Class: 
+class LinearRegression:
+    def __init__(self, learning_rate: float = 0.01, max_epochs: int = 1000, tolerance: float = 1e-6):
+        pass
+
+    def forward(self, X, **kwargs):
+        pass 
+
+    def train(**kwargs):
+        pass
+
+    def predict(**kwargs):
+        pass
+
+    def test(**kwargs):
+        pass
+
+    def plot_parameters(**kwargs):
+        pass
+
